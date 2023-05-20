bbf4f494c7db52065f40ea352ac4614e8d86eb0b
Kamran Sadiq <kamransadiq111@gmail.com>
2023-05-21 01:15:14 +0500

    changes

--- Content/js/main.js
+++ Content/js/main.js
@@ -1,6 +1,9 @@
 jQuery(document).on('ready', function ($) {
 	"use strict";
 
+
+ 
+
 	/*--------------------------
 	    SCROLLSPY ACTIVE
 	---------------------------*/
@@ -214,6 +217,27 @@ jQuery(document).on('ready', function ($) {
 	    ACTIVE WOW JS
 	----------------------------*/
 	new WOW().init();
+
+	$('.customer-logos').slick({
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 1500,
+		arrows: false,
+		dots: false,
+		pauseOnHover: false,
+		responsive: [{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 4
+			}
+		}, {
+			breakpoint: 520,
+			settings: {
+				slidesToShow: 3
+			}
+		}]
+	});
 
 }(jQuery));
 
